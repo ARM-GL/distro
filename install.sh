5cafbb14493aa0dc72f42d0ab6017cfba6dace40
Igor Rudyk <rudyk.igor@gmail.com>
2021-12-14 13:25:10 +0200

    Support aarch64

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [[ "$THIS_DIR" == *" "* ]]; then
 Please try a different path, one without any spaces."
     exit 1
 fi
-PREFIX=${PREFIX:-"${THIS_DIR}/install"}
+PREFIX=${PREFIX:-"/usr/local"}
 TORCH_LUA_VERSION=${TORCH_LUA_VERSION:-"LUAJIT21"} # by default install LUAJIT21
 
 while getopts 'bsh' x; do
@@ -67,12 +67,19 @@ if [[ `uname -a` == *"ARCH"* ]]; then
 fi
 
 echo "Installing Lua version: ${TORCH_LUA_VERSION}"
-mkdir -p install
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DWITH_${TORCH_LUA_VERSION}=ON 2>&1 >>$PREFIX/install.log || exit 1
-(make 2>&1 >>$PREFIX/install.log  || exit 1) && (make install 2>&1 >>$PREFIX/install.log || exit 1)
+git clone https://luajit.org/git/luajit.git
+cd luajit
+make && make install
+ln -sf luajit-2.1.0-beta3 /usr/local/bin/luajit
 cd ..
+apt-get install luarocks
+
+#mkdir -p install
+#mkdir -p build
+#cd build
+#cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DWITH_${TORCH_LUA_VERSION}=ON 2>&1 >>$PREFIX/install.log || exit 1
+#(make 2>&1 >>$PREFIX/install.log  || exit 1) && (make install 2>&1 >>$PREFIX/install.log || exit 1)
+#cd ..
 
 # Check for a CUDA install (using nvcc instead of nvidia-smi for cross-platform compatibility)
 path_to_nvcc=$(which nvcc)
@@ -108,49 +115,49 @@ setup_lua_env_cmd=$($PREFIX/bin/luarocks path)
 eval "$setup_lua_env_cmd"
 
 echo "Installing common Lua packages"
-cd ${THIS_DIR}/extra/luafilesystem && $PREFIX/bin/luarocks make rockspecs/luafilesystem-1.6.3-1.rockspec || exit 1
-cd ${THIS_DIR}/extra/penlight && $PREFIX/bin/luarocks make penlight-scm-1.rockspec || exit 1
-cd ${THIS_DIR}/extra/lua-cjson && $PREFIX/bin/luarocks make lua-cjson-2.1devel-1.rockspec || exit 1
+cd ${THIS_DIR}/extra/luafilesystem && luarocks make rockspecs/luafilesystem-1.6.3-1.rockspec || exit 1
+cd ${THIS_DIR}/extra/penlight && luarocks make penlight-scm-1.rockspec || exit 1
+cd ${THIS_DIR}/extra/lua-cjson && luarocks make lua-cjson-2.1devel-1.rockspec || exit 1
 
 echo "Installing core Torch packages"
-cd ${THIS_DIR}/extra/luaffifb && $PREFIX/bin/luarocks make luaffi-scm-1.rockspec       || exit 1
-cd ${THIS_DIR}/pkg/sundown   && $PREFIX/bin/luarocks make rocks/sundown-scm-1.rockspec || exit 1
-cd ${THIS_DIR}/pkg/cwrap     && $PREFIX/bin/luarocks make rocks/cwrap-scm-1.rockspec   || exit 1
-cd ${THIS_DIR}/pkg/paths     && $PREFIX/bin/luarocks make rocks/paths-scm-1.rockspec   || exit 1
-cd ${THIS_DIR}/pkg/torch     && $PREFIX/bin/luarocks make rocks/torch-scm-1.rockspec   || exit 1
-cd ${THIS_DIR}/pkg/dok       && $PREFIX/bin/luarocks make rocks/dok-scm-1.rockspec     || exit 1
-cd ${THIS_DIR}/exe/trepl     && $PREFIX/bin/luarocks make trepl-scm-1.rockspec         || exit 1
-cd ${THIS_DIR}/pkg/sys       && $PREFIX/bin/luarocks make sys-1.1-0.rockspec           || exit 1
-cd ${THIS_DIR}/pkg/xlua      && $PREFIX/bin/luarocks make xlua-1.0-0.rockspec          || exit 1
-cd ${THIS_DIR}/extra/moses   && $PREFIX/bin/luarocks make rockspec/moses-1.6.1-1.rockspec || exit 1
-cd ${THIS_DIR}/extra/nn      && $PREFIX/bin/luarocks make rocks/nn-scm-1.rockspec      || exit 1
-cd ${THIS_DIR}/extra/graph   && $PREFIX/bin/luarocks make rocks/graph-scm-1.rockspec   || exit 1
-cd ${THIS_DIR}/extra/nngraph && $PREFIX/bin/luarocks make nngraph-scm-1.rockspec       || exit 1
-cd ${THIS_DIR}/pkg/image     && $PREFIX/bin/luarocks make image-1.1.alpha-0.rockspec   || exit 1
-cd ${THIS_DIR}/pkg/optim     && $PREFIX/bin/luarocks make optim-1.0.5-0.rockspec       || exit 1
+cd ${THIS_DIR}/extra/luaffifb && luarocks make luaffi-scm-1.rockspec       || exit 1
+cd ${THIS_DIR}/pkg/sundown   && luarocks make rocks/sundown-scm-1.rockspec || exit 1
+cd ${THIS_DIR}/pkg/cwrap     && luarocks make rocks/cwrap-scm-1.rockspec   || exit 1
+cd ${THIS_DIR}/pkg/paths     && luarocks make rocks/paths-scm-1.rockspec   || exit 1
+cd ${THIS_DIR}/pkg/torch     && luarocks make rocks/torch-scm-1.rockspec   || exit 1
+cd ${THIS_DIR}/pkg/dok       && luarocks make rocks/dok-scm-1.rockspec     || exit 1
+cd ${THIS_DIR}/exe/trepl     && luarocks make trepl-scm-1.rockspec         || exit 1
+cd ${THIS_DIR}/pkg/sys       && luarocks make sys-1.1-0.rockspec           || exit 1
+cd ${THIS_DIR}/pkg/xlua      && luarocks make xlua-1.0-0.rockspec          || exit 1
+cd ${THIS_DIR}/extra/moses   && luarocks make rockspec/moses-1.6.1-1.rockspec || exit 1
+cd ${THIS_DIR}/extra/nn      && luarocks make rocks/nn-scm-1.rockspec      || exit 1
+cd ${THIS_DIR}/extra/graph   && luarocks make rocks/graph-scm-1.rockspec   || exit 1
+cd ${THIS_DIR}/extra/nngraph && luarocks make nngraph-scm-1.rockspec       || exit 1
+cd ${THIS_DIR}/pkg/image     && luarocks make image-1.1.alpha-0.rockspec   || exit 1
+cd ${THIS_DIR}/pkg/optim     && luarocks make optim-1.0.5-0.rockspec       || exit 1
 
 if [ -x "$path_to_nvcc" ]
 then
     echo "Found CUDA on your machine. Installing CUDA packages"
-    cd ${THIS_DIR}/extra/cutorch && $PREFIX/bin/luarocks make rocks/cutorch-scm-1.rockspec || exit 1
-    cd ${THIS_DIR}/extra/cunn    && $PREFIX/bin/luarocks make rocks/cunn-scm-1.rockspec    || exit 1
+    cd ${THIS_DIR}/extra/cutorch && luarocks make rocks/cutorch-scm-1.rockspec || exit 1
+    cd ${THIS_DIR}/extra/cunn    && luarocks make rocks/cunn-scm-1.rockspec    || exit 1
 fi
 
 # Optional packages
 echo "Installing optional Torch packages"
-cd ${THIS_DIR}/pkg/gnuplot          && $PREFIX/bin/luarocks make rocks/gnuplot-scm-1.rockspec
-cd ${THIS_DIR}/exe/env              && $PREFIX/bin/luarocks make env-scm-1.rockspec
-cd ${THIS_DIR}/extra/nnx            && $PREFIX/bin/luarocks make nnx-0.1-1.rockspec
-cd ${THIS_DIR}/exe/qtlua            && $PREFIX/bin/luarocks make rocks/qtlua-scm-1.rockspec
-cd ${THIS_DIR}/pkg/qttorch          && $PREFIX/bin/luarocks make rocks/qttorch-scm-1.rockspec
-cd ${THIS_DIR}/extra/threads        && $PREFIX/bin/luarocks make rocks/threads-scm-1.rockspec
-cd ${THIS_DIR}/extra/argcheck       && $PREFIX/bin/luarocks make rocks/argcheck-scm-1.rockspec
+cd ${THIS_DIR}/pkg/gnuplot          && luarocks make rocks/gnuplot-scm-1.rockspec
+cd ${THIS_DIR}/exe/env              && luarocks make env-scm-1.rockspec
+cd ${THIS_DIR}/extra/nnx            && luarocks make nnx-0.1-1.rockspec
+cd ${THIS_DIR}/exe/qtlua            && luarocks make rocks/qtlua-scm-1.rockspec
+cd ${THIS_DIR}/pkg/qttorch          && luarocks make rocks/qttorch-scm-1.rockspec
+cd ${THIS_DIR}/extra/threads        && luarocks make rocks/threads-scm-1.rockspec
+cd ${THIS_DIR}/extra/argcheck       && luarocks make rocks/argcheck-scm-1.rockspec
 
 # Optional CUDA packages
 if [ -x "$path_to_nvcc" ]
 then
     echo "Found CUDA on your machine. Installing optional CUDA packages"
-    cd ${THIS_DIR}/extra/cudnn   && $PREFIX/bin/luarocks make cudnn-scm-1.rockspec
+    cd ${THIS_DIR}/extra/cudnn   && luarocks make cudnn-scm-1.rockspec
 fi
 
 export PATH=$OLDPATH # Restore anaconda distribution if we took it out.
